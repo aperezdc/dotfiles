61f39bdb5c87f277d37630a0d6a71ca26586de72
Adrian Perez <aperez@igalia.com>
2012-05-18 17:25:23 +0300

    my-notmuch.el: Update for new Lisp API in notmuch 0.13

--- emacs/my-notmuch.el
+++ emacs/my-notmuch.el
@@ -23,78 +23,78 @@
 							"toggle deleted tag for thread"
 							(interactive)
 							(if (member "deleted" (notmuch-search-get-tags))
-								(notmuch-search-tag-thread "-deleted")
-								(notmuch-search-tag-thread "+deleted" "-inbox" "-unread"))))
+								(notmuch-search-tag "-deleted")
+								(notmuch-search-tag '("+deleted" "-inbox" "-unread")))))
 
 (define-key notmuch-show-mode-map "d"
 						(lambda ()
 							"toggle deleted tag for message"
 							(interactive)
 							(if (member "deleted" (notmuch-show-get-tags))
-								(notmuch-show-tag-message "-deleted")
-								(notmuch-show-tag-message "+deleted" "-inbox" "-unread"))))
+								(notmuch-show-tag "-deleted")
+								(notmuch-show-tag '("+deleted" "-inbox" "-unread")))))
 
 (define-key notmuch-search-mode-map "j"
 						(lambda ()
 							"toggle the spam (junk) tag for thread"
 							(interactive)
 							(if (member "spam" (notmuch-search-get-tags))
-								(notmuch-search-tag-thread "-spam")
-								(notmuch-search-tag-thread "+spam" "-ham" "-inbox" "-unread"))))
+								(notmuch-search-tag "-spam")
+								(notmuch-search-tag '("+spam" "-ham" "-inbox" "-unread")))))
 
 (define-key notmuch-show-mode-map "j"
 						(lambda ()
 							"toggle the spam (junk) tag for message"
 							(interactive)
 							(if (member "spam" (notmuch-show-get-tags))
-								(notmuch-show-tag-message "-spam")
-								(notmuch-show-tag-message "+spam" "-ham" "-inbox" "-unread"))))
+								(notmuch-show-tag "-spam")
+								(notmuch-show-tag '("+spam" "-ham" "-inbox" "-unread")))))
 
 (define-key notmuch-search-mode-map "H"
 						(lambda ()
 							"toggle the ham tag for thread"
 							(interactive)
 							(if (member "ham" (notmuch-search-get-tags))
-								(notmuch-search-tag-thread "-ham")
-								(notmuch-search-tag-thread "+ham" "-spam"))))
+								(notmuch-search-tag "-ham")
+								(notmuch-search-tag '("+ham" "-spam")))))
 
 (define-key notmuch-show-mode-map "H"
 						(lambda ()
 							"toggle the ham tag for message"
 							(interactive)
 							(if (member "ham" (notmuch-show-get-tags))
-								(notmuch-show-tag-message "-ham")
-								(notmuch-show-tag-message "+ham" "-ham"))))
+								(notmuch-show-tag "-ham")
+								(notmuch-show-tag '("+ham" "-ham")))))
 
-(define-key notmuch-search-mode-map "f"
+(define-key notmuch-search-mode-map "F"
 						(lambda ()
 							"toggle the flagged tag for thread"
 							(interactive)
 							(if (member "flagged" (notmuch-search-get-tags))
-								(notmuch-search-tag-thread "-flagged")
-								(notmuch-search-tag-thread "+flagged"))))
+								(notmuch-search-tag "-flagged")
+								(notmuch-search-tag "+flagged"))))
 
-(define-key notmuch-show-mode-map "f"
+(define-key notmuch-show-mode-map "F"
 						(lambda ()
 							"toggle the flagged tag for message"
 							(interactive)
 							(if (member "flagged" (notmuch-show-get-tags))
-								(notmuch-show-tag-message "-flagged")
-								(notmuch-show-tag-message "+flagged"))))
+								(notmuch-show-tag "-flagged")
+								(notmuch-show-tag "+flagged"))))
 
 (define-key notmuch-search-mode-map "i"
 						(lambda ()
 							"toggle the ignore tag for thread"
 							(interactive)
 							(if (member "ignore" (notmuch-search-get-tags))
-								(notmuch-search-tag-thread "-ignore")
-								(notmuch-search-tag-thread "+ignore" "-inbox" "-unread"))))
+								(notmuch-search-tag "-ignore")
+								(notmuch-search-tag '("+ignore" "-inbox" "-unread")))))
 
 (define-key notmuch-show-mode-map "i"
 						(lambda ()
 							"toggle the ignore tag for message"
 							(interactive)
 							(if (member "ignore" (notmuch-show-get-tags))
-								(notmuch-show-tag-message "-ignore")
-								(notmuch-show-tag-message "+ignore" "-inbox" "-unread"))))
+								(notmuch-show-tag "-ignore")
+								(notmuch-show-tag '("+ignore" "-inbox" "-unread")))))
 
